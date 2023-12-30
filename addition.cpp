6ae1f3475ca06e4f27f04bfa258e9a98b196055b
Mann2006 <145533502+Mann2006@users.noreply.github.com>
2023-12-30 09:41:10 +0530

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int x , y ;
+
+    cout<<"enter your first number:";
+    cin>>x;
+
+    cout<<"enter your second number:";
+    cin>>y;
+
+    cout<<"ANSWER IS :"<<x+y;
+
+
+    return 0;
+
+
+
+}
