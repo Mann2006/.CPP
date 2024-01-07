3ebdd22881c0568466cbc8dee67b23427de22d4c
Mann2006 <145533502+Mann2006@users.noreply.github.com>
2024-01-07 12:58:39 +0530

    Add files via upload

---
+++ relational.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    
+    int a=3,b=5;
+    int res;
+
+    res=(a==b);
+    cout<<res<<endl;
+
+    res=a>b;
+    cout<<res<<endl;
+
+    res=a<b;
+    cout<<res<<endl;
+
+    return 0;
+}
